74fb91e98dd958ab6027aa9bf43e8b5459c9c7ac
Marques <marques.johnson1992@gmail.com>
2019-09-16 16:14:05 -0400

    commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const server = express();
+
+server.use(express.json());
+
+const PORT = process.env.PORT || 4000
+
+server.listen(PORT, () => {
+    console.log(`server listening on ${PORT}`)
+})
